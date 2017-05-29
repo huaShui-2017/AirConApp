9f0fab24c4a4c5d3939e7df4c8e4dda42193288a
zjp <944593237@qq.com>
2017-05-29 14:07:40 +0800

    添加接口调用

---
+++ Air/js/req.js
@@ -0,0 +1,16 @@
+var httpReq = {
+	getData: function(doSuccess){
+		mui.ajax('http://47.92.108.119:80/spring-mybatis-demo/getCust/202',{
+			dataType:'json',//服务器返回json格式数据
+			type:'post',//HTTP请求类型
+			timeout:10000,//超时时间设置为10秒；
+			headers:{'Content-Type':'application/json'},	              
+			success:function(data){
+				doSuccess(data);
+			},
+			error:function(xhr,type,errorThrown){
+				mui.toast("请求空调数据失败！");
+			}
+		});
+	}
+}
